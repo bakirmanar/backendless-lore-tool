3f16a262dc89e769391edef9f4d9b58e58de872b
Manar Bakir <bakir.manar.m@gmail.com>
2026-06-15 21:16:22 +0200

    Streamlined signals for ArticleListComponent

--- src/app/components/article-list/article-list.component.ts
+++ src/app/components/article-list/article-list.component.ts
@@ -17,31 +17,25 @@ export class ArticleListComponent {
   protected readonly stateService: StateService = inject(StateService);
   private readonly router: Router = inject(Router);
 
-  private articles: Signal<Article[]>;
+  private articles: Signal<Article[]> = this.stateService.articles;
+  protected readonly filtered: Signal<Article[]> = computed(() => {
+    const query = (this.searchValue() || '').toLowerCase();
+    const type = this.typeValue();
+
+    return this.articles().filter(article => {
+      const matchTitle = !query || article.title?.toLowerCase().includes(query);
+      const matchType = type == null || article.type === type;
+      return matchTitle && matchType;
+    });
+  });
 
   // Filters
   protected readonly ArticleType = ArticleType;
   protected readonly search = new FormControl<string>('', { nonNullable: true });
   protected readonly typeFilter = new FormControl<ArticleType | null>(null);
-  protected readonly filtered: Signal<Article[]>;
+
   private readonly searchValue = toSignal(this.search.valueChanges.pipe(startWith(this.search.value)), { initialValue: this.search.value });
   private readonly typeValue = toSignal(this.typeFilter.valueChanges.pipe(startWith(this.typeFilter.value)), { initialValue: this.typeFilter.value });
-
-  constructor() {
-    // TODO onInit or onAfterInit
-    this.articles = this.stateService.articles;
-
-    this.filtered = computed(() => {
-      const query = (this.searchValue() || '').toLowerCase();
-      const type = this.typeValue();
-
-      return this.articles().filter(article => {
-        const matchTitle = !query || article.title?.toLowerCase().includes(query);
-        const matchType = type == null || article.type === type;
-        return matchTitle && matchType;
-      });
-    });
-  }
 
   goCreate() {
     this.router.navigate(['/create']);
