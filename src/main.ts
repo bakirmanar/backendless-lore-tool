ccdeb1ade9352b8601d00d2defe5f9278e3ab783
Manar Bakir <bakir.manar.m@gmail.com>
2026-09-21 19:03:35 +0200

    Upgrade to Angular 21 (due to autosizeCdk bug)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
+import { provideZoneChangeDetection } from '@angular/core';
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app-module';
 
 //ng generate c NAME --style=scss --v=none --type=component
 //ng generate s name --type=service
 
-platformBrowser().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
-})
-  .catch(err => console.error(err));
+platformBrowser()
+  .bootstrapModule(AppModule, {
+    applicationProviders: [provideZoneChangeDetection({ eventCoalescing: true })],
+  })
+  .catch((err) => console.error(err));
